8fe964e8d6a64c39e68084a9db28746956829825
Vitold Meshcheryakov <Vitold.Meshcheryakov@dataart.com>
2014-08-25 10:28:30 +0000

    Exception handling

---
+++ src/main/java/com/dataart/spring/controllers/ExceptionHandlingController.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.dataart.spring.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author vmeshcheryakov
+ *
+ */
+@ControllerAdvice
+public class ExceptionHandlingController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlingController.class);
+
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleError(HttpServletRequest req, Exception exception) {
+		LOGGER.error("Request: " + req.getRequestURL() + " raised " + exception);
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", exception);
+		mav.addObject("url", req.getRequestURL());
+		mav.setViewName("error");
+		return mav;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,26 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<%@ page session="true" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Sign up</title>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	</head>
+	<body>
+		<%@ include file="fragments/menu-light.jsp" %>
+		<div class="container">
+			<div class="jumbotron">
+				<h3>
+					Something go wrong...
+				</h3>
+			</div>
+		</div>
+		
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</body>
+</html>
