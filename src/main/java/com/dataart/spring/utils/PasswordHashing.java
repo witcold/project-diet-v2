c471a7bd628c0ebf3a9407aaaa40fbcff4f0276f
Vitold Meshcheryakov <Vitold.Meshcheryakov@dataart.com>
2014-08-26 09:45:06 +0000

    Deleting unused code

--- src/main/java/com/dataart/spring/utils/PasswordHashing.java
+++ src/main/java/com/dataart/spring/utils/PasswordHashing.java
@@ -14,8 +14,6 @@ import org.apache.commons.codec.binary.Hex;
  */
 public class PasswordHashing {
 
-//	public static final String SALT_STRING = "dataart";
-	// = encode("pass") with commented code
 	public static final String SALT_STRING = "1243418ab49d816ddb58c9b5b7fba0308af8132d";
 
 	public static String encode(String data) {
@@ -24,13 +22,7 @@ public class PasswordHashing {
 			md = MessageDigest.getInstance("SHA-1");
 		} catch (NoSuchAlgorithmException e) {
 		}
-//		byte[] hash = md.digest(data.getBytes());
-//		byte[] hashsalt = md.digest(SALT_STRING.getBytes());
-//		byte[] result = new byte[hash.length + hashsalt.length];
-//		System.arraycopy(hash, 0, result, 0, hash.length);
-//		System.arraycopy(hashsalt, 0, result, hash.length, hashsalt.length);
-//		return new String(Hex.encodeHex(md.digest(result)));
-		return new String(Hex.encodeHex(md.digest((data+SALT_STRING).getBytes())));
+		return new String(Hex.encodeHex(md.digest((data + SALT_STRING).getBytes())));
 	}
 
 	public static boolean check(String data, String hash) {
