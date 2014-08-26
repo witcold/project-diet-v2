5a7222503419668aa58e740f901f3579fde061a1
Vitold Meshcheryakov <Vitold.Meshcheryakov@dataart.com>
2014-08-26 14:29:29 +0000

    Changed DB path

---
+++ sql/scheme.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+	login			VARCHAR(20) NOT NULL,
+	password		VARCHAR(80) NOT NULL,
+	first_name		VARCHAR(20) NOT NULL,
+	last_name		VARCHAR(20) NOT NULL,
+	PRIMARY KEY (login)
+);
+
