5b5388eef523303776c17ab804e4c3ee559474f8
Vitold Meshcheryakov <Vitold.Meshcheryakov@dataart.com>
2014-09-22 09:34:46 +0000

    Added small populate scripts

---
+++ sql/categories_data.sql
@@ -0,0 +1,18 @@
+﻿INSERT INTO categories
+	(parent_id, name_en, name_ru)
+VALUES
+	(0, 'Vegetables', 'Овощи'),
+	(0, 'Fruits and berries', 'Фрукты и ягоды'),
+	(0, 'Mushrooms', 'Грибы'),
+	(0, 'Eggs and dairy products', 'Яйца и молочные продукты'),
+	(0, 'Meat products', 'Мясные продукты'),
+	(0, 'Fish', 'Рыба и морепродукты'),
+	(0, 'Nuts and dried fruits', 'Орехи и сухофрукты'),
+	(0, 'Flour and flour products', 'Мука и мучные изделия'),
+	(0, 'Cereals and porridge', 'Крупы и каши'),
+	(0, 'Confectionery and sweets', 'Кондитерские изделия и сладости'),
+	(0, 'Spices', 'Специи и пряности'),
+	(0, 'Raw materials and additives', 'Сырье и добавки'),
+	(0, 'baby food', 'Детское питание'),
+	(0, 'Soft drinks', 'Безалкогольные напитки'),
+	(0, 'Alcoholic beverages', 'Алкогольные напитки');
---
+++ sql/foods_data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO foods
+	(category_id, name_en, name_ru, calories, proteins, fats, carbohydrates)
+VALUES
+	(15, 'Water', 'Вода', 0, 0, 0, 0);
