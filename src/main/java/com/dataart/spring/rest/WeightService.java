740583f1ed6654da6c4a0acb41eb6464c5106cc1
Vitold Meshcheryakov <Vitold.Meshcheryakov@dataart.com>
2014-10-16 13:05:53 +0000

    Added WeightService

---
+++ src/main/java/com/dataart/spring/rest/WeightService.java
@@ -0,0 +1,50 @@
+package com.dataart.spring.rest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dataart.spring.dao.WeightDAO;
+import com.dataart.spring.model.User;
+import com.dataart.spring.model.Weight;
+import com.dataart.spring.utils.DateUtils;
+
+/**
+ * @author vmeshcheryakov
+ *
+ */
+@RestController
+@RequestMapping("/weights")
+public class WeightService {
+
+	@Autowired
+	private WeightDAO weightDAO;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
+	}
+
+	@RequestMapping(value = "/{fromdate}/{todate}", method = RequestMethod.GET)
+	public List<Weight> get(
+			@PathVariable("fromdate") Date from,
+			@PathVariable("todate") Date to,
+			HttpSession session) {
+		User user = (User) session.getAttribute("account");
+		return weightDAO.selectByUserIdWithRange(user.getId(),
+				DateUtils.getFirstDayOfMonth(from),
+				DateUtils.getLastDayOfMonth(to));
+	}
+
+}
